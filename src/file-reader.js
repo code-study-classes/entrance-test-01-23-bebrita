b9bd5c5cae799b61d119eea6cab9f33864ea4676
Yanoka2010 <146944149+Yanoka2010@users.noreply.github.com>
2024-09-12 06:34:42 +0000

    paha

--- src/file-reader.js
+++ src/file-reader.js
@@ -1,6 +1,11 @@
 import getPath from './get-path.js';
 import fs from 'fs';
 
-const filePath = getPath('data/regions.csv');
-const fileData = fs.readFileSync(filePath, 'utf-8');
-console.log(fileData);
+const reader = (fPath) =>{ 
+    const filePath = getPath(fPath);
+    const fileData = fs.readFileSync(filePath, 'utf-8');
+    console.log(fileData);
+    return fileData;
+}
+
+export default reader;
--- src/file-updater.js
+++ src/file-updater.js
@@ -1,7 +1,10 @@
 import getPath from "./get-path.js";
 import fs from 'fs';
 import readlineSync from 'readline-sync';
+import reader from "./file-reader.js";
 
 const filePath = getPath('data/regions.csv');
-const newData = readlineSync.question('New region:');
-fs.appendFileSync(filePath, `\n${newData}`, 'utf-8');
+const newData = readlineSync.question('New region:').toLowerCase().trim();
+const oldDate = reader('data/regions.csv').length;
+console.log(oldDate);
+fs.appendFileSync(filePath, `\n${id}|${newData}`, 'utf-8');
